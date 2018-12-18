71f201e95d76a5b14f170808bf66213925d87487
Mark Gituma <gitumarkk@gmail.com>
2018-12-18 03:47:02 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a5283e31ef44e7c2559e.js"
+    "url": "webpack-runtime-942e510e3354276666e0.js"
   },
   {
     "url": "app-2f749f57efc7c6a16c04.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f9161e725180c9e89a02c90e4cf1c82b"
+    "revision": "138691ffb35086ccfe250fda3d9a6231"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
